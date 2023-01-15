9986ae67e219f652216047dbb1d7226e7ed9d86d
lelis <lelis685@gmail.com>
2023-01-15 20:05:58 -0300

    test: melhorando design DateTimeConverterTest

--- src/test/java/br/com/patientintake/DateTimeConverterTest.java
+++ src/test/java/br/com/patientintake/DateTimeConverterTest.java
@@ -1,5 +1,7 @@
 package br.com.patientintake;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
@@ -8,39 +10,50 @@ import java.time.LocalDateTime;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@DisplayName("DateTimeConverter")
 class DateTimeConverterTest {
 
+    @Nested
+    @DisplayName("convert string with 'today' keyword")
+    class TodayTests {
+        @Test
+        @DisplayName("correctly")
+        void convertTodayStringCorrectly() {
+            LocalDate today = LocalDate.of(2018, 9, 1);
+            LocalDateTime result = DateTimeConverter.convertStringToDateTime("today 1:00 pm",
+                    today);
+            assertEquals(result, LocalDateTime.of(2018, 9, 1, 13, 0),
+                    () -> "Failed to convert 'today' string to expected date time, today passed was: " + today);
+        }
 
-    @Test
-    void convertTodayStringCorrectly(){
-        LocalDateTime result = DateTimeConverter.convertStringToDateTime("today 1:00 pm",
-                LocalDate.of(2023, 1,15));
-
-        assertEquals(result, LocalDateTime.of(2023, 1,15,13,0));
+        @Test
+        @DisplayName("regardless of case")
+        void convertTodayStringCorrectlyCaseInsensitive() {
+            LocalDate today = LocalDate.of(2018, 9, 1);
+            LocalDateTime result = DateTimeConverter.convertStringToDateTime("ToDay 1:00 pm",
+                    today);
+            assertEquals(result, LocalDateTime.of(2018, 9, 1, 13, 0),
+                    () -> "Failed to convert 'today' string to expected date time, today passed was: " +today);
+        }
     }
 
     @Test
-    void convertCorrectPatternDateTime(){
-        LocalDateTime result = DateTimeConverter.convertStringToDateTime("1/15/2023 1:00 pm",
-                LocalDate.of(2023, 1,15));
-
-        assertEquals(result, LocalDateTime.of(2023, 1,15,13,0));
+    @DisplayName("convert expected date time pattern in string correctly")
+    void convertCorrectPatternToDateTime() {
+        LocalDateTime result = DateTimeConverter.convertStringToDateTime("9/2/2018 1:00 pm",
+                LocalDate.of(2018, 9, 1));
+        assertEquals(result, LocalDateTime.of(2018, 9, 2, 13, 0));
     }
 
-
     @Test
-    void throwExceptionIfIncorrectPatternProvided(){
-
-        String dateTimeStringInvalid = "1/99/2023 1:00 pm";
-        RuntimeException runtimeException = assertThrows(RuntimeException.class, () -> {
-            DateTimeConverter.convertStringToDateTime(dateTimeStringInvalid,
-                    LocalDate.of(2023, 1, 15));
-        });
-
-        assertEquals("Unable to create date time from: [" +
-                dateTimeStringInvalid + "], please enter with format [M/d/yyyy h:mm a], Text '1/99/2023 1:00 PM' could not be parsed: Invalid value for DayOfMonth (valid values 1 - 28/31): 99",
-                runtimeException.getMessage());
-
+    @DisplayName("throw exception if entered pattern of string incorrect")
+    void throwExceptionIfIncorrectPatternProvided() {
+        Throwable error = assertThrows(RuntimeException.class, () ->
+                DateTimeConverter.convertStringToDateTime("9/2/2018 100 pm",
+                        LocalDate.of(2018, 9, 1)));
+        assertEquals("Unable to create date time from: [9/2/2018 100 pm], " +
+                "please enter with format [M/d/yyyy h:mm a], Text '9/2/2018 100 PM' " +
+                "could not be parsed at index 12", error.getMessage());
     }
 
 
